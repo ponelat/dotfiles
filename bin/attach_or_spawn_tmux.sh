a43dc1458f69eeac3c65a851aaf39089e439e21e
Josh Ponelat <josh@ponelat.com>
2015-10-23 16:38:11 +0200

    update tmux to use new-session -t 0 and to agressive resize

--- bin/attach_or_spawn_tmux.sh
+++ bin/attach_or_spawn_tmux.sh
@@ -3,4 +3,7 @@
 TMUX="/home/josh/.linuxbrew/bin/tmux"
 
 # Try to attach to tmux, failing that spawn new instance
-$TMUX -2 attach || $TMUX -2
+# $TMUX -2 attach || $TMUX -2
+
+# Create a new session, attach to '0' if it exists
+$TMUX -2 new-session -t '0' || $TMUX -2 new-session -s '0'
