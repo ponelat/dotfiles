f9d7cfb73bb8195c68e22da0e68aa8b76fa0a6d1
Josh Ponelat <josh@ponelat.com>
2016-08-29 12:37:53 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -1,32 +1,14 @@
 #!/usr/bin/env bash
+echo Installing dotfiles... will quit on first error
+set -e
+thisDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-DOTS="$DIR/dots"
-homefiles=`find $DOTS` 
-CONFIG=${XDG_CONFIG_HOME:=$HOME/.config}
-NVIM_HOME="$CONFIG/nvim"
-
-echo  Link all homefiles
-
-# Each file on a separate line.
-for _dotfile in $homefiles
-do
-  echo  ...Linking $_dotfile
-  ln -i "$_dotfile" "$HOME/`basename $_dotfile`" # interactive, hard links
-done
-
-
-if [[ ! -d $HOME/.vim ]]; then
-  echo 'making .vim'
-  mkdir $HOME/.vim
-fi
+# Link the dotfiles
+rcup -d $thisDir/dots -v
 
 # Setup nvim as links to vim
+CONFIG=${XDG_CONFIG_HOME:=$HOME/.config}
+NVIM_HOME="$CONFIG/nvim"
 [ ! -d "$NVIM_HOME" ] && ln -s "$HOME/.vim" "$NVIM_HOME"
-ln "$DOTS/.vimrc" "$NVIM_HOME/init.vim" 
+[ ! -f "$NVIM_HOME/init.vim" ] && ln "$HOME/.vimrc" "$NVIM_HOME/init.vim" 
 
-# Create link for bin
-[ ! -d "$HOME/bin" ] && ln -s "$DIR/bin" "$HOME/bin"  # interactive
-
-# echo Install gnome-terminal theme
-# see: https://github.com/chriskempson/base16-builder
