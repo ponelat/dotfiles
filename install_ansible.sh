cd7772ad57ae6279f4c0e34f0bc67650b779f520
Josh Ponelat <josh@ponelat.com>
2016-08-29 12:52:16 +0200

    silly safe guard to install_ansible

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,6 +1,8 @@
-#sudo apt-add-repository -y ppa:ansible/ansible
-#sudo apt-get update
-#sudo apt-get install -y ansible
-#sudo mv /etc/ansible/hosts /etc/ansible/hosts.orig
-echo -e "localhost ansible_connection=local" | sudo tee /etc/ansible/hosts
+if ! dpkg-query -W ansible ; then
+  sudo apt-add-repository -y ppa:ansible/ansible
+  sudo apt-get update
+  sudo apt-get install -y ansible
+  sudo mv /etc/ansible/hosts /etc/ansible/hosts.orig
+  echo -e "localhost ansible_connection=local" | sudo tee /etc/ansible/hosts
+fi
 
