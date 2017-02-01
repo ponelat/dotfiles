f1cb554bd2a07ee78f953e051c5fb95b471a4487
Josh Ponelat <josh@ponelat.com>
2017-02-01 21:23:28 +0200

    remove project-bins

--- bin/project-bins.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Echo project bin folders, that we want in the path
-proj="$HOME/projects"
-
-echo $proj/docker-cloud
-echo $proj/scripts
-echo /home/josh/apps/robomongo-0.9.0-rc9-linux-x86_64-0bb5668/bin
