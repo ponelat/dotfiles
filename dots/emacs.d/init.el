d1141f9efd125a7e389e631809095ded93ce2a79
Josh Ponelat <josh@ponelat.com>
2017-02-09 11:25:57 +0200

    add x copy paste to emacs

--- dots/emacs.d/init.el
+++ dots/emacs.d/init.el
@@ -9,10 +9,66 @@
       inhibit-startup-message t
       inhibit-startup-echo-area-message t)
 
-;;;; init.el helpers
-(defmacro comment (&rest body)
-  "Comment out sexp"
-  nil)
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(tool-bar-mode -1)
+
+;;;; Clipboard
+
+;; https://hugoheden.wordpress.com/2009/03/08/copypaste-with-emacs-in-terminal/
+;; I prefer using the "clipboard" selection (the one the
+;; typically is used by c-c/c-v) before the primary selection
+;; (that uses mouse-select/middle-button-click)
+(setq x-select-enable-clipboard t)
+
+;; If emacs is run in a terminal, the clipboard- functions have no
+;; effect. Instead, we use of xsel, see
+;; http://www.vergenet.net/~conrad/software/xsel/ -- "a command-line
+;; program for getting and setting the contents of the X selection"
+p(unless window-system
+  (when (getenv "DISPLAY")
+    ;; Callback for when user cuts
+    (defun xsel-cut-function (text &optional push)
+      ;; Insert text to temp-buffer, and "send" content to xsel stdin
+      (with-temp-buffer
+	(insert text)
+	;; I prefer using the "clipboard" selection (the one the
+	;; typically is used by c-c/c-v) before the primary selection
+	;; (that uses mouse-select/middle-button-click)
+	(call-process-region (point-min) (point-max) "xsel" nil 0 nil "--clipboard" "--input")))
+    ;; Call back for when user pastes
+    (defun xsel-paste-function()
+      ;; Find out what is current selection by xsel. If it is different
+      ;; from the top of the kill-ring (car kill-ring), then return
+      ;; it. Else, nil is returned, so whatever is in the top of the
+      ;; kill-ring will be used.
+      (let ((xsel-output (shell-command-to-string "xsel --clipboard --output")))
+	(unless (string= (car kill-ring) xsel-output)
+	  xsel-output )))
+    ;; Attach callbacks to hooks
+    (setq interprogram-cut-function 'xsel-cut-function)
+    (setq interprogram-paste-function 'xsel-paste-function)
+    ;; Idea from
+    ;; http://shreevatsa.wordpress.com/2006/10/22/emacs-copypaste-and-x/
+    ;; http://www.mail-archive.com/help-gnu-emacs@gnu.org/msg03577.html
+    ))
+
+
+;;;; Themes
+(use-package ample-theme
+  :init (progn (load-theme 'ample t t)
+	       (load-theme 'ample-flat t t)
+	       (load-theme 'ample-light t t)
+	       (enable-theme 'ample))
+  :defer t
+  :ensure t)
+
+;;;; Autosave
+;; store all backup and autosave files in the tmp dir
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
 
 ;;;; Clojure
 (use-package cider
