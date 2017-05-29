34aab8c932a85acbbe13b3f2b5f4c25ece6c3bd8
Josh Ponelat <jponelat@gmail.com>
2017-05-29 08:26:23 +0200

    update layout

--- dots/config/i3/layout.sh
+++ dots/config/i3/layout.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-i3-msg "workspace 1; append_layout ~/.config/i3/workspace_1.json"
-i3-msg "workspace 2; append_layout ~/.config/i3/workspace_2.json"
-i3-msg "workspace 3; append_layout ~/.config/i3/workspace_3.json"
+i3-msg "workspace 1; append_layout ~/.config/i3/workspace_term.json"
+i3-msg "workspace 2; append_layout ~/.config/i3/workspace_emacs.json"
+i3-msg "workspace 3; append_layout ~/.config/i3/workspace_chrome.json"
 
 # And finally we fill the containers with the programs they had
 (emacsclient -c -a '' &)
 (i3-sensible-terminal &)
 (chrome &)
-
-# Just a bonus
-(startvpn &)
