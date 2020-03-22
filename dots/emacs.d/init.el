04a9f0f4005ceb385a67ed7e6bf87d02c5bc0019
Josh Ponelat <jponelat@gmail.com>
2020-03-22 21:11:28 +0200

    emacs: multiedit and slime-nav

--- dots/emacs.d/init.el
+++ dots/emacs.d/init.el
@@ -603,6 +603,46 @@ eg: \"Hello over there\" => \"hello-over-there\"
   :config (progn (global-set-key (kbd "C-c +") 'evil-numbers/inc-at-pt)
             (global-set-key (kbd "C-c -") 'evil-numbers/dec-at-pt)))
 
+(use-package evil-multiedit
+  :config (progn
+            ;; Highlights all matches of the selection in the buffer.
+            (define-key evil-visual-state-map "R" 'evil-multiedit-match-all)
+
+            ;; Match the word under cursor (i.e. make it an edit region). Consecutive presses will
+            ;; incrementally add the next unmatched match.
+            (define-key evil-normal-state-map (kbd "M-d") 'evil-multiedit-match-and-next)
+            ;; Match selected region.
+            (define-key evil-visual-state-map (kbd "M-d") 'evil-multiedit-match-and-next)
+            ;; Insert marker at point
+            (define-key evil-insert-state-map (kbd "M-d") 'evil-multiedit-toggle-marker-here)
+
+            ;; Same as M-d but in reverse.
+            (define-key evil-normal-state-map (kbd "M-D") 'evil-multiedit-match-and-prev)
+            (define-key evil-visual-state-map (kbd "M-D") 'evil-multiedit-match-and-prev)
+
+            ;; OPTIONAL: If you prefer to grab symbols rather than words, use
+            ;; `evil-multiedit-match-symbol-and-next` (or prev).
+
+            ;; Restore the last group of multiedit regions.
+            (define-key evil-visual-state-map (kbd "C-M-D") 'evil-multiedit-restore)
+
+            ;; RET will toggle the region under the cursor
+            (define-key evil-multiedit-state-map (kbd "RET") 'evil-multiedit-toggle-or-restrict-region)
+
+            ;; ...and in visual mode, RET will disable all fields outside the selected region
+            (define-key evil-motion-state-map (kbd "RET") 'evil-multiedit-toggle-or-restrict-region)
+
+            ;; For moving between edit regions
+            (define-key evil-multiedit-state-map (kbd "C-n") 'evil-multiedit-next)
+            (define-key evil-multiedit-state-map (kbd "C-p") 'evil-multiedit-prev)
+            (define-key evil-multiedit-insert-state-map (kbd "C-n") 'evil-multiedit-next)
+            (define-key evil-multiedit-insert-state-map (kbd "C-p") 'evil-multiedit-prev)
+
+            ;; Ex command that allows you to invoke evil-multiedit with a regular expression, e.g.
+            (evil-ex-define-cmd "ie[dit]" 'evil-multiedit-ex-match
+
+              )))
+
 (use-package evil-collection
   :after evil
   :custom (evil-collection-setup-minibuffer t)
@@ -714,6 +754,9 @@ eg: \"Hello over there\" => \"hello-over-there\"
 ;; (define-key key-translation-map (kbd "ESC") (kbd "C-g"))
 
 ;;;; Lisp, paredit
+(use-package elisp-slime-nav
+  :config (add-hook 'emacs-lisp-mode-hook 'turn-on-elisp-slime-nav-mode))
+
 (show-paren-mode 1)
 (use-package parinfer
   :disabled t
