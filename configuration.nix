e4578c5f4d25f11faa5600b2fa11bb9cb2060f38
Josh Ponelat <jponelat@gmail.com>
2021-06-11 11:04:50 +0200

    nix: add nix cli

--- configuration.nix
+++ configuration.nix
@@ -98,6 +98,12 @@ in {
   # Docker
   virtualisation.docker.enable = true;
 
+  # Enable nix eval --expr
+  nix.package = pkgs.nixUnstable;
+  nix.extraOptions = ''
+      experimental-features = nix-command
+   '';
+
   environment.systemPackages = with pkgs; [
     curl wget vim git fasd jq sqlite unzip ripgrep xsel
 
